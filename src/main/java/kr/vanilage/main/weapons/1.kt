25047a78792decafd800455bc3fabae692d282de
MAKEREKAM <kimyounghyun123456@gmail.com>
2024-04-03 20:11:10 +0900

    Update

--- src/main/java/kr/vanilage/main/weapons/1.kt
+++ src/main/java/kr/vanilage/main/weapons/1.kt
@@ -3,12 +3,12 @@ package kr.vanilage.main.weapons
 import kr.vanilage.main.Weapon
 import org.bukkit.entity.Player
 
-class `1`(override val coolTick: Int) : Weapon{
+class `1`(override val coolTick : Int) : Weapon{
     override fun setup() {
 
     }
 
-    override fun skill(player: Player) {
+    override fun skill(player : Player) {
 
     }
 }
