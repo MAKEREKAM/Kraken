0f681d19424239a5c84d74fe17764d78ef3b2c5d
MAKEREKAM <kimyounghyun123456@gmail.com>
2024-05-16 23:45:58 +0900

    Update

--- src/main/java/kr/vanilage/main/weapons/11.kt
+++ src/main/java/kr/vanilage/main/weapons/11.kt
@@ -1,17 +1,7 @@
 package kr.vanilage.main.weapons
 
-import kr.vanilage.main.Main
 import kr.vanilage.main.Weapon
-import org.bukkit.Bukkit
-import org.bukkit.Material
-import org.bukkit.Particle
 import org.bukkit.entity.*
-import org.bukkit.inventory.ItemStack
-import org.bukkit.potion.PotionEffect
-import org.bukkit.potion.PotionEffectType
-import org.bukkit.util.Transformation
-import org.joml.AxisAngle4f
-import org.joml.Vector3f
 
 class `11`(override val coolTick : Int) : Weapon{
     override fun setup() {
@@ -19,102 +9,9 @@ class `11`(override val coolTick : Int) : Weapon{
     }
 
     override fun skill(player : Player) {
-        val bullet = player.world.spawn(player.location, Bee::class.java)
-        bullet.addScoreboardTag("10Bullet")
+        val bullet = player.world.spawn(player.location, Snowball::class.java)
+        bullet.addScoreboardTag("11Bullet")
         bullet.isVisibleByDefault = false
-        bullet.isInvulnerable = true
-        bullet.isSilent = true
-        bullet.addPotionEffect(
-            PotionEffect(
-                PotionEffectType.SLOW,
-                PotionEffect.INFINITE_DURATION,
-                255,
-                false,
-                false
-            )
-        )
-        bullet.isSilent = true
 
-        val entities = player.getNearbyEntities(50.0, 50.0, 50.0)
-
-        val filteredEntity = entities.filter { it.uniqueId != player.uniqueId && it.uniqueId != bullet.uniqueId && it is LivingEntity } as ArrayList<Entity>
-        filteredEntity.sortWith(compareBy { it.location.distanceSquared(player.location) })
-
-        val attack = object : Runnable {
-            override fun run() {
-                if (filteredEntity.isNotEmpty() && !bullet.isDead) {
-                    val attackEntity = filteredEntity[0]
-
-                    val attackEntityRunnable = this
-
-                    val listSize = filteredEntity.size
-
-                    Bukkit.getScheduler().runTaskLater(Main.instance, Runnable {
-                        if (listSize == filteredEntity.size) {
-                            bullet.remove()
-                        }
-                    }, 100)
-
-                    val setVelocity = object : Runnable {
-                        override fun run() {
-                            bullet.velocity = attackEntity.location.toVector()
-                                .subtract(bullet.location.toVector()).normalize().multiply(3.5)
-
-                            if (bullet.location.distance(attackEntity.location) >= 7) {
-                                Bukkit.getScheduler().runTaskLater(Main.instance, this, 5)
-                            }
-
-                            else {
-                                (attackEntity as LivingEntity).damage(3.0)
-                                filteredEntity.remove(attackEntity)
-
-                                attackEntity.world.playSound(
-                                    attackEntity.location,
-                                    "kraken:10attack",
-                                    3F, 1F
-                                )
-
-                                Bukkit.getScheduler().runTaskLater(Main.instance, attackEntityRunnable, 5)
-                            }
-                        }
-                    }
-
-                    Bukkit.getScheduler().runTask(Main.instance, setVelocity)
-                }
-
-                else {
-                    bullet.remove()
-                }
-            }
-        }
-
-        val spawnParticle = object : Runnable {
-            override fun run() {
-                if (!bullet.isDead) {
-                    bullet.world.spawnParticle(
-                        Particle.CRIT,
-                        bullet.location,
-                        10, 0.2, 0.2, 0.2, 0.0, null, true
-                    )
-
-                    bullet.world.spawnParticle(
-                        Particle.FLAME,
-                        bullet.location,
-                        10, 0.2, 0.2, 0.2, 0.0, null, true
-                    )
-
-                    bullet.world.spawnParticle(
-                        Particle.ELECTRIC_SPARK,
-                        bullet.location,
-                        10, 0.2, 0.2, 0.2, 0.0, null, true
-                    )
-
-                    Bukkit.getScheduler().runTaskLater(Main.instance, this, 1)
-                }
-            }
-        }
-
-        Bukkit.getScheduler().runTask(Main.instance, attack)
-        Bukkit.getScheduler().runTask(Main.instance, spawnParticle)
     }
 }
