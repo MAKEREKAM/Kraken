aa2dd10ec19c7b8c509dbab26efd646884804a4d
MAKEREKAM <kimyounghyun123456@gmail.com>
2024-05-18 00:27:21 +0900

    Update

--- src/main/java/kr/vanilage/main/weapons/11.kt
+++ src/main/java/kr/vanilage/main/weapons/11.kt
@@ -4,10 +4,15 @@ import kr.vanilage.main.Main
 import kr.vanilage.main.Weapon
 import org.bukkit.Bukkit
 import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.entity.*
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.ProjectileHitEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.util.Transformation
+import org.joml.AxisAngle4f
+import org.joml.Vector3f
 
 class `11`(override val coolTick : Int) : Weapon{
     override fun setup() {
@@ -30,6 +35,21 @@ class `11`(override val coolTick : Int) : Weapon{
                     if (e.entity.uniqueId == bullet.uniqueId) {
                         val location = e.hitBlock!!.location
                         val distance = location.distance(player.location)
+
+                        val snowball = player.launchProjectile(Snowball::class.java)
+                        snowball.isVisibleByDefault = false
+                        snowball.setGravity(false)
+                        snowball.velocity = player.location.direction.normalize().multiply(distance / 20)
+
+                        val display = snowball.world.spawn(player.location, ItemDisplay::class.java)
+                        display.itemStack = ItemStack(Material.NETHERITE_AXE)
+                        display.teleportDuration = 1
+                        display.transformation = Transformation(
+                            Vector3f(0F, 0F, 0F),
+                            AxisAngle4f(1.5707963F, 0F, 1F, 0F),
+                            Vector3f(1F, 1F, 1F),
+                            AxisAngle4f(1F, 0F, 0F, 0F)
+                        )
                     }
                 }
             }, Main.instance
